ed6ec683a78dd9b2a6d02ed6f6edc563cd5bfffd
AkshayShivajiMatre08 <78848364+AkshayShivajiMatre08@users.noreply.github.com>
2021-02-10 19:37:32 +0530

    Text To Speech Part Added

---
+++ texttospeech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+speaker = pyttsx3.init()
+speaker.say("Hey man , Listen, there is an black scorpio , crossing the road .with a speed of fifty km per hour.  at a distance 200 meter ,at a left side from you. Please stop for five minute, and then,  you can go straight, thank you")
+speaker.runAndWait()
